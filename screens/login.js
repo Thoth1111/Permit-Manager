f7e51e8fbe02761de829ec55906d1ee9476629c0
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-21 15:00:31 +0300

    Complete build of login screen

--- screens/login.js
+++ screens/login.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
+
+// Styled Components
+import MyTextInput from '../components/MyTextInput.js';
+import { Container, InnerContainer, AppLogo, PageTitle, SubHeading, FormArea, StyledButton, ButtonText, Colors, MsgBox,
+    Line, ExtraView, ExtraText, TextLink, TextLinkContent
+} from '../components/styles';
+
+// Colors
+const { platinum } = Colors;
 
 // Formik
 import { Formik } from 'formik';
 
-import {
-    Container,
-    InnerContainer,
-    AppLogo,
-    PageTitle,
-    SubHeading,
-    FormArea
-} from '../components/styles';
-
 const Login = () => {
+    const [hidePassword, setHidePassword] = useState(true)
+
     return (
         <Container>
             <StatusBar style="light" />
@@ -22,14 +24,47 @@ const Login = () => {
                 <PageTitle>COUNTY LICENSE MANAGER</PageTitle>
                 <SubHeading>Sign In</SubHeading>
                 <Formik
-                    initialValues = {{ email:'', password:'' }}
+                    initialValues = {{ id_number:'', password:'' }}
                     onSubmit = {(values) => {
                         console.log(values);
                     }}
                 >
-                    {(handleChange, handleBlur, handleSubmit, values) => (
+                    {({handleChange, handleBlur, handleSubmit, values}) => (
                         <FormArea>
-
+                            <MyTextInput
+                                label="Id Number"
+                                placeholder="e.g 12345678"
+                                placeholderTextColor={platinum}
+                                iconName="User"
+                                onChangeText={handleChange('id_number')}
+                                onBlur={handleBlur('id_number')}
+                                value={values.id_number}
+                                keyboardType="numeric"
+                            />
+                            <MyTextInput
+                                label="Password"
+                                placeholder="* * * * * * * *"
+                                placeholderTextColor={platinum}
+                                iconName="Key"
+                                onChangeText={handleChange('password')}
+                                onBlur={handleBlur('password')}
+                                value={values.password}
+                                isPassword={true}
+                                secureTextEntry={hidePassword}
+                                hidePassword={hidePassword}
+                                setHidePassword={setHidePassword}
+                            />
+                            <MsgBox>...</MsgBox>
+                            <StyledButton onPress={handleSubmit}>
+                                <ButtonText>Login</ButtonText>
+                            </StyledButton>
+                            <Line />
+                            <ExtraView>
+                                <ExtraText>Don't have an account?</ExtraText>
+                                <TextLink>
+                                    <TextLinkContent>Sign Up</TextLinkContent>
+                                </TextLink>
+                            </ExtraView>
                         </FormArea>
                     )}
                 </Formik>
@@ -37,5 +72,7 @@ const Login = () => {
         </Container>
     )
 }
+
+
 
 export default Login;
