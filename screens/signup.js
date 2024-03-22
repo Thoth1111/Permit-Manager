588af209e4520fb14945dfd1125cd3fe03c9c5ed
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-22 13:21:58 +0300

    Update SignUp Screen

--- screens/signup.js
+++ screens/signup.js
@@ -6,87 +6,93 @@ import MyTextInput from '../components/MyTextInput.js';
 import { Formik } from 'formik';
 
 // Styled Components
-import { Container, InnerContainer, AppLogo, PageTitle, SubHeading, FormArea, StyledButton, ButtonText, Colors, MsgBox,
+import {
+    Container, InnerContainer, AppLogo, PageTitle, SubHeading, FormArea, StyledButton, ButtonText, Colors, MsgBox,
     Line, ExtraView, ExtraText, TextLink, TextLinkContent
 } from '../components/styles';
 
-// Colors
-const { platinum } = Colors;
+// Keyboard Averse Wrapper
+import KeyboardAverseWrapper from '../components/KeyboardAverseWrapper.js';
 
-const SignUp = () => {
+// Colors
+const { platinum, green } = Colors;
+
+const SignUp = ({ navigation }) => {
     const [hidePassword, setHidePassword] = useState(true)
 
     return (
-        <Container>
-            <StatusBar style="light" />
-            <InnerContainer>
-                <PageTitle>COUNTY LICENSE MANAGER</PageTitle>
-                <SubHeading>Register</SubHeading>
-                <Formik
-                    initialValues = {{ id_number:'', phone_number:'', password:'', confirm_password:'' }}
-                    onSubmit = {(values) => {
-                        console.log(values);
-                    }}
-                >
-                    {({handleChange, handleBlur, handleSubmit, values}) => (
-                        <FormArea>
-                            <MyTextInput
-                                label="Id Number"
-                                placeholder="e.g 12345678"
-                                placeholderTextColor={platinum}
-                                iconName="User"
-                                onChangeText={handleChange('id_number')}
-                                onBlur={handleBlur('id_number')}
-                                value={values.id_number}
-                                keyboardType="numeric"
-                            />
-                            <MyTextInput
-                                label="Phone Number"
-                                placeholder="e.g 0712345678"
-                                placeholderTextColor={platinum}
-                                iconName="Smartphone"
-                                onChangeText={handleChange('phone_number')}
-                                onBlur={handleBlur('phone_number')}
-                                value={values.phone_number}
-                                keyboardType="numeric"
-                            />
-                            <MyTextInput
-                                label="Password"
-                                placeholder="* * * * * * * *"
-                                placeholderTextColor={platinum}
-                                iconName="Key"
-                                onChangeText={handleChange('password')}
-                                onBlur={handleBlur('password')}
-                                value={values.password}
-                                isPassword={true}
-                                secureTextEntry={hidePassword}
-                                hidePassword={hidePassword}
-                                setHidePassword={setHidePassword}
-                            />
-                            <MyTextInput
-                                label="Confirm Password"
-                                iconName="Key"
-                                onChangeText={handleChange('confirm_password')}
-                                onBlur={handleBlur('confirm_password')}
-                                value={values.confirm_password}
-                                secureTextEntry={true}
-                            />
-                            <MsgBox>...</MsgBox>
-                            <StyledButton onPress={handleSubmit}>
-                                <ButtonText>Register</ButtonText>
-                            </StyledButton>
-                            <Line />
-                            <ExtraView>
-                                <ExtraText>Already have an account?</ExtraText>
-                                <TextLink>
-                                    <TextLinkContent>Sign In</TextLinkContent>
-                                </TextLink>
-                            </ExtraView>
-                        </FormArea>
-                    )}
-                </Formik>
-            </InnerContainer>
-        </Container>
+        <KeyboardAverseWrapper>
+            <Container color={green}>
+                <StatusBar style="light" />
+                <InnerContainer color={green}>
+                    <PageTitle>COUNTY LICENSE MANAGER</PageTitle>
+                    <SubHeading>Register</SubHeading>
+                    <Formik
+                        initialValues={{ id_number: '', phone_number: '', password: '', confirm_password: '' }}
+                        onSubmit={(values) => {
+                            console.log(values);
+                        }}
+                    >
+                        {({ handleChange, handleBlur, handleSubmit, values }) => (
+                            <FormArea>
+                                <MyTextInput
+                                    label="Id Number"
+                                    placeholder="e.g 12345678"
+                                    placeholderTextColor={platinum}
+                                    iconName="User"
+                                    onChangeText={handleChange('id_number')}
+                                    onBlur={handleBlur('id_number')}
+                                    value={values.id_number}
+                                    keyboardType="numeric"
+                                />
+                                <MyTextInput
+                                    label="Phone Number"
+                                    placeholder="e.g 0712345678"
+                                    placeholderTextColor={platinum}
+                                    iconName="Smartphone"
+                                    onChangeText={handleChange('phone_number')}
+                                    onBlur={handleBlur('phone_number')}
+                                    value={values.phone_number}
+                                    keyboardType="numeric"
+                                />
+                                <MyTextInput
+                                    label="Password"
+                                    placeholder="* * * * * * * *"
+                                    placeholderTextColor={platinum}
+                                    iconName="Key"
+                                    onChangeText={handleChange('password')}
+                                    onBlur={handleBlur('password')}
+                                    value={values.password}
+                                    isPassword={true}
+                                    secureTextEntry={hidePassword}
+                                    hidePassword={hidePassword}
+                                    setHidePassword={setHidePassword}
+                                />
+                                <MyTextInput
+                                    label="Confirm Password"
+                                    iconName="Key"
+                                    onChangeText={handleChange('confirm_password')}
+                                    onBlur={handleBlur('confirm_password')}
+                                    value={values.confirm_password}
+                                    secureTextEntry={true}
+                                />
+                                <MsgBox>...</MsgBox>
+                                <StyledButton onPress={handleSubmit}>
+                                    <ButtonText>Register</ButtonText>
+                                </StyledButton>
+                                <Line />
+                                <ExtraView>
+                                    <ExtraText>Already have an account?</ExtraText>
+                                    <TextLink onPress={()=> navigation.navigate('Login')}>
+                                        <TextLinkContent>Sign In</TextLinkContent>
+                                    </TextLink>
+                                </ExtraView>
+                            </FormArea>
+                        )}
+                    </Formik>
+                </InnerContainer>
+            </Container>
+        </KeyboardAverseWrapper>
     )
 }
 
