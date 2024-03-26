c8bcaf7128f73259a10cfea12df337ef400a55d0
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-26 03:17:29 +0300

    Update SignUp Screen

--- screens/signup.js
+++ screens/signup.js
@@ -7,7 +7,7 @@ import { Formik } from 'formik';
 
 // Styled Components
 import {
-    Container, InnerContainer, AppLogo, PageTitle, SubHeading, FormArea, StyledButton, ButtonText, Colors, MsgBox,
+    Container, InnerContainer, PageTitle, SubHeading, FormArea, StyledButton, ButtonText, Colors, MsgBox,
     Line, ExtraView, ExtraText, TextLink, TextLinkContent
 } from '../components/styles.js';
 
@@ -28,7 +28,7 @@ const SignUp = ({ navigation }) => {
                     <PageTitle>COUNTY LICENSE MANAGER</PageTitle>
                     <SubHeading>Register</SubHeading>
                     <Formik
-                        initialValues={{ id_number: '', phone_number: '', password: '', confirm_password: '' }}
+                        initialValues={{ email: '', national_id_number: '', phone_number: '', password: '', confirm_password: '' }}
                         onSubmit={(values) => {
                             console.log(values);
                         }}
@@ -36,13 +36,23 @@ const SignUp = ({ navigation }) => {
                         {({ handleChange, handleBlur, handleSubmit, values }) => (
                             <FormArea>
                                 <MyTextInput
-                                    label="Id Number"
+                                    label="Email"
+                                    placeholder=""
+                                    placeholderTextColor={platinum}
+                                    iconName="Mail"
+                                    onChangeText={handleChange('email')}
+                                    onBlur={handleBlur('email')}
+                                    value={values.email}
+                                    keyboardType="email"
+                                />
+                                <MyTextInput
+                                    label="National Id Number"
                                     placeholder="e.g 12345678"
                                     placeholderTextColor={platinum}
                                     iconName="User"
-                                    onChangeText={handleChange('id_number')}
-                                    onBlur={handleBlur('id_number')}
-                                    value={values.id_number}
+                                    onChangeText={handleChange('national_id_number')}
+                                    onBlur={handleBlur('national_id_number')}
+                                    value={values.national_id_number}
                                     keyboardType="numeric"
                                 />
                                 <MyTextInput
