d6c8bcb9fd85468f429cfc650051686248a456f1
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-03-26 04:04:31 +0300

    Update Login Screen

--- screens/login.js
+++ screens/login.js
@@ -4,7 +4,7 @@ import { StatusBar } from 'expo-status-bar';
 // Styled Components
 import MyTextInput from '../components/MyTextInput.js';
 import {
-    Container, InnerContainer, AppLogo, PageTitle, SubHeading, FormArea, StyledButton, ButtonText, Colors, MsgBox,
+    Container, InnerContainer, PageTitle, SubHeading, FormArea, StyledButton, ButtonText, Colors, MsgBox,
     Line, ExtraView, ExtraText, TextLink, TextLinkContent
 } from '../components/styles.js';
 
@@ -25,26 +25,25 @@ const Login = ({ navigation }) => {
             <Container color={green}>
                 <StatusBar style="light" />
                 <InnerContainer color={green}>
-                    <AppLogo resizeMode="cover" source={require('../assets/images/cityLogo.jpg')} />
-                    <PageTitle>COUNTY LICENSE MANAGER</PageTitle>
+                    <PageTitle>BUSINESS LICENSE MANAGER</PageTitle>
                     <SubHeading>Sign In</SubHeading>
                     <Formik
-                        initialValues={{ id_number: '', password: '' }}
+                        initialValues={{ national_id_number: '', password: '' }}
                         onSubmit={(values) => {
                             console.log(values);
                             navigation.navigate('Home')
                         }}
                     >
                         {({ handleChange, handleBlur, handleSubmit, values }) => (
-                            <FormArea>
+                            <FormArea style={{ marginTop: '15%' }}>
                                 <MyTextInput
-                                    label="Id Number"
+                                    label="National Id Number"
                                     placeholder="e.g 12345678"
                                     placeholderTextColor={platinum}
                                     iconName="User"
-                                    onChangeText={handleChange('id_number')}
-                                    onBlur={handleBlur('id_number')}
-                                    value={values.id_number}
+                                    onChangeText={handleChange('national_id_number')}
+                                    onBlur={handleBlur('national_id_number')}
+                                    value={values.national_id_number}
                                     keyboardType="numeric"
                                 />
                                 <MyTextInput
@@ -61,19 +60,19 @@ const Login = ({ navigation }) => {
                                     setHidePassword={setHidePassword}
                                 />
                                 <MsgBox>...</MsgBox>
-                                <StyledButton onPress={handleSubmit}>
+                                <StyledButton onPress={handleSubmit} style={{ marginTop: '5%' }}>
                                     <ButtonText>Login</ButtonText>
                                 </StyledButton>
-                                <Line />
-                                <ExtraView>
-                                    <ExtraText>Don't have an account?</ExtraText>
-                                    <TextLink onPress={()=> navigation.navigate('SignUp')}>
-                                        <TextLinkContent>Sign Up</TextLinkContent>
-                                    </TextLink>
-                                </ExtraView>
                             </FormArea>
                         )}
                     </Formik>
+                    <Line style={{ marginTop: '55%' }}/>
+                    <ExtraView>
+                        <ExtraText>Don't have an account?</ExtraText>
+                        <TextLink onPress={() => navigation.navigate('SignUp')}>
+                            <TextLinkContent>Sign Up</TextLinkContent>
+                        </TextLink>
+                    </ExtraView>
                 </InnerContainer>
             </Container>
         </KeyboardAverseWrapper>
