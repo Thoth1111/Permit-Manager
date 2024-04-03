81462a4033da61884d866a02227546843ce414cf
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-03 16:24:54 +0300

    Update userRequests.js

--- API/userRequests.js
+++ API/userRequests.js
@@ -6,14 +6,16 @@ const createAccount = async (values) => {
     await axios.post(`${URL}/user/register`, values)
         .then((res) => {
             console.log(res);
-            // if (response.status === 201) {
-            //     setLoading(false);
+            if (response.status === 201) {
+                setLoading(false);
+                alert(`Success! ${response.refreshtoken}`)
             //     console.log(response.status);
             //     navigation.navigate('Home');
             // } else {
-                setLoading(false);
+                // setLoading(false);
             //     alert('Error logging in. Check server logs')
-            // }
+            }
+            alert(res.status)
         })
         .catch((err) => {
             console.log(err);
@@ -26,14 +28,16 @@ const loginUser = async (values) => {
     await axios.post(`${URL}/user/login`, values)
                 .then((res) => {
                     console.log(res);
-                    // if (response.status === 200) {
+                    if (response.status === 200) {
                     //     setLoading(false);
                     //     console.log(response.status);
                     //     navigation.navigate('Home');
                     // } else {
+                        alert(`Success! ${response.refreshtoken}`)
                         setLoading(false);
                     //     alert('Error logging in. Check server logs')
-                    // }
+                    }
+                    alert(res.status)
                 })
                 .catch((err) => {
                     console.log(err);
