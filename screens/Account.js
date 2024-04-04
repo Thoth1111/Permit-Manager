c924ad3cb3c40000d334567c17291b0e1e413eef
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-04 17:44:45 +0300

    Update Account Screen

--- screens/Account.js
+++ screens/Account.js
@@ -1,17 +1,40 @@
-import React from 'react'
+import React, { useState, useContext } from 'react'
 import * as Icon from 'react-native-feather'
 import { Container, InnerContainer, PageTitle, CardContainer, Line, StyledButton, ButtonText, Colors } from '../components/styles'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+import { UserContext } from '../components/UserContext'
+import { logoutUser } from '../API/userRequests'
 
 const { green } = Colors
 
 const Account = ({ navigation }) => {
+  const [loading, setLoading] = useState(false)
+  const { userData, setUserData } = useContext(UserContext);
+  const { national_id_number } = userData
+
+  const clearUserData = () => {
+    AsyncStorage.removeItem('userSessionData')
+    .then(() => {
+      setUserData(null)
+    })
+    .catch((err) => {
+      console.log(err)
+    })
+  }
+
+  const handleLogout = () => {
+    setLoading(true);
+    logoutUser(national_id_number, clearUserData, setLoading);
+  }
+
   return (
     <Container>
         <InnerContainer>
             <PageTitle color={green}>Account</PageTitle>
             <CardContainer>
                 <Line />
-                <StyledButton onPress={() => navigation.navigate('Login')} style={{ flexDirection: 'row' }}>
+                <StyledButton onPress={handleLogout} style={{ flexDirection: 'row' }}>
                     <Icon.LogOut size={50} color={green} />
                     <ButtonText>Log Out</ButtonText>
                 </StyledButton>
