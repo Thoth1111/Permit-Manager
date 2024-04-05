57031433fb4261818a363bf3c08bb1bdc2f25b56
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-05 21:54:52 +0300

    Update MyTextInput Component

--- components/MyTextInput.js
+++ components/MyTextInput.js
@@ -1,31 +1,12 @@
 import React from 'react';
-import { View } from 'react-native';
-import * as Icon from 'react-native-feather';
-import { StyledInputLabel, StyledTextInput, LeftIcon, RightIcon, Colors } from './styles';
+import { StyledInputLabel, StyledTextInput, FittedContainer } from './styles';
 
-const { platinum, green } = Colors;
-
-const MyTextInput = ({label, iconName, isPassword, hidePassword, setHidePassword, ...props}) => {
-    const DynamicIcon = Icon[iconName];
-    const EyeOn = Icon.Eye;
-    const EyeOff = Icon.EyeOff;
+const MyTextInput = ({label, ...props}) => {
     return (
-        <View>
-            <LeftIcon>
-                <DynamicIcon size={25} color={platinum} />
-            </LeftIcon>
+        <FittedContainer>
             <StyledInputLabel>{label}</StyledInputLabel>
             <StyledTextInput {...props} />
-            {isPassword && (
-                <RightIcon onPress={()=> setHidePassword(!hidePassword)}>
-                    { hidePassword ? (
-                        <EyeOff size={30} color={platinum} />
-                    ) : (
-                        <EyeOn size={30} color={green} />
-                    )}
-                </RightIcon>
-            )}
-        </View>
+        </FittedContainer>
     )
 }
 
