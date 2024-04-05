8831b6ec109b5a8cb74957d0188a98092c1612ba
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-05 21:55:08 +0300

    Update validations helper

--- helpers/validations.js
+++ helpers/validations.js
@@ -4,6 +4,7 @@ export const validateNationalId = (national_id_number) => {
         return true;
     } else {
         console.log('National Id Number must be 8 numeric characters long');
+        alert('National Id Number must be 8 numeric characters long');
         return false;
     };
 }
@@ -14,6 +15,7 @@ export const validateEmail = (email) => {
         return true;
     } else {
         console.log('Invalid email address');
+        alert('Invalid email address');
         return false;
     };
 }
@@ -24,6 +26,7 @@ export const validatePhoneNumber = (phone_number) => {
         return true;
     } else {
         console.log('Phone number must be 10 numeric characters long and start with 0');
+        alert('Phone number must be 10 numeric characters long and start with 0');
         return false;
     };
 }
@@ -35,7 +38,19 @@ export const validatePassword = (password, confirm_password) => {
         return false;
     } else if (!passwordRegex.test(password)) {
         console.log('Password must be at least 8 characters long and contain at least one uppercase letter, one number and one special character');
+        alert('Password must be at least 8 characters long and contain at least one uppercase letter, one number and one special character');
         return false;
     }
     return true;
+}
+
+export const validateBusinessId = (business_id) => {
+    const businessIdRegex = /^[0-9]+$/;
+    if (businessIdRegex.test(business_id)) {
+        return true;
+    } else {
+        console.log('Business Id Number must be numeric characters');
+        alert('Business Id Number must be numeric characters');
+        return false;
+    };
 }
