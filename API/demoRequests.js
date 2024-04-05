7052ea809b3f7afa43ceac6d19c8f6031a17d78a
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-05 21:52:54 +0300

    Add demo data for licenses

---
+++ API/demoRequests.js
@@ -0,0 +1,87 @@
+const Stacy1 = {
+    user_id: "23456789",
+    license_info: {
+        business_name: "Muchira's Wine & Spirits",
+        business_id: "4143",
+        kra_pin: "A12345678",
+        activity_code: "895",
+        effective_date: "2023-03-26",
+        expiry_date: "2024-03-26",
+        location: {
+            constituency: "Roysambu",
+            ward: "Kahawa",
+            plot_number: "120",
+            road_street: "k-road",
+            building: "",
+            floor: "",
+            stall_number: ""
+        }
+    }
+}
+
+const Stacy2 = {
+    user_id: "23456789",
+    license_info: {
+        business_name: "Pub Muchacho",
+        business_id: "4302",
+        kra_pin: "A22345678",
+        activity_code: "894",
+        effective_date: "2022-12-22",
+        expiry_date: "2023-12-22",
+        location: {
+            constituency: "Roysambu",
+            ward: "Githurai",
+            plot_number: "3850",
+            road_street: "g-road",
+            building: "",
+            floor: "",
+            stall_number: ""
+        }
+    }
+}
+
+const Benson1 = {
+    user_id: "12345678",
+    license_info: {
+        business_name: "Fanzyy Eats & Drinks",
+        business_id: "7043",
+        kra_pin: "A32345678",
+        activity_code: "942",
+        effective_date: "2023-04-20",
+        expiry_date: "2024-04-20",
+        location: {
+            constituency: "Westlands",
+            ward: "Mountain View",
+            plot_number: "6330",
+            road_street: "mv-road",
+            building: "",
+            floor: "",
+            stall_number: ""
+        }
+    }
+}
+
+const Benson2 = {
+    user_id: "12345678",
+    license_info: {
+        business_name: "Kangemi Merchants Club",
+        business_id: "8653",
+        kra_pin: "A42345678",
+        activity_code: "1268",
+        effective_date: "2023-03-26",
+        expiry_date: "2024-03-26",
+        location: {
+            constituency: "Westlands",
+            ward: "Kangemi",
+            plot_number: "2250",
+            road_street: "kg-road",
+            building: "",
+            floor: "",
+            stall_number: ""
+        }
+    }
+}
+
+export const demoLicenses = [
+    Stacy1, Stacy2, Benson1, Benson2
+]
