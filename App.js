cc36c68371bc820950e6fe4f28250e44c1e6460a
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-06 04:23:56 +0300

    Update App.js

--- App.js
+++ App.js
@@ -1,11 +1,9 @@
 import React, { useState, useEffect } from 'react';
-// React Navigation Stack
 import RootStack from './navigators/RootStack';
-// Async Storage
 import AsyncStorage from '@react-native-async-storage/async-storage';
-// User Context
+import { Provider } from 'react-redux';
+import store from './redux/store'
 import { UserContext } from './components/UserContext';
-// Splash Screen
 import Splash from './components/Splash';
 
 export default function App({ navigation }) {
@@ -13,14 +11,11 @@ export default function App({ navigation }) {
   const [userData, setUserData] = useState(null);
 
   const checkUserData = async () => {
-    console.log('Checking user data')
     await AsyncStorage.getItem('userSessionData')
       .then((userSessionData) => {
         if (userSessionData !== null) {
-          console.log('User data found')
           setUserData(JSON.parse(userSessionData))
         } else {
-          console.log('No user data found')
           setUserData(null)
         }
       })
@@ -28,7 +23,6 @@ export default function App({ navigation }) {
         console.log(err)
       })
       .finally(() => {
-        console.log('final callback executed')
         setAppReady(true)
       })
   }
@@ -42,8 +36,10 @@ export default function App({ navigation }) {
   }
 
   return (
-    <UserContext.Provider value={({ userData, setUserData })}>
-      <RootStack />
-    </UserContext.Provider>
+    <Provider store={store}>
+      <UserContext.Provider value={({ userData, setUserData })}>
+        <RootStack />
+      </UserContext.Provider>
+    </Provider>
   );
 }
