2b11c1134dc7a209d07b4d0e09307be381420e9f
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-06 04:26:36 +0300

    Update Licenses Screen

--- screens/Licenses.js
+++ screens/Licenses.js
@@ -1,13 +1,17 @@
-import React, { useState, useContext } from 'react'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 import * as Icon from 'react-native-feather'
-import { Container, InnerContainer, PageTitle, SubHeading, ListsView, Line, Colors, CardContainer, FittedContainer, FloatingButton, StyledButton, ButtonText } from '../components/styles'
+import { Container, InnerContainer, PageTitle, SubHeading, ListsView, Line, Colors, CardContainer, FittedContainer,
+    FloatingButton, StyledButton, ButtonText, CardView, CardTitle 
+} from '../components/styles'
 import LicenseForm from '../components/LicenseForm';
 
 const { green, platinum } = Colors;
 
 const Licenses = () => {
     const [newLicense, setNewLicense] = useState(false)
-    
+    const Licenses = useSelector(state => state.licenses)
+
     const closeForm = () => {
         setNewLicense(false)
     }
@@ -23,7 +27,7 @@ const Licenses = () => {
                             <FloatingButton onPress={() => setNewLicense(false)} >
                                 <Icon.X size={30} color={green} />
                             </FloatingButton>
-                            <LicenseForm closeForm={closeForm}/>
+                            <LicenseForm closeForm={closeForm} />
                         </FittedContainer>
                     ) : (
                         <StyledButton onPress={() => setNewLicense(true)} style={{ flexDirection: 'row' }}>
@@ -35,6 +39,17 @@ const Licenses = () => {
                 </CardContainer>
                 <ListsView>
                     <SubHeading color={platinum} >Saved Licenses</SubHeading>
+                    {Licenses.length > 0 && Licenses.map((license, i) => (
+                        <FittedContainer key={i}>
+                            {/* <Line /> */}
+                            {/* <CardView onPress={() => navigation.navigate('License', { license })}> */}
+                            <CardView>
+                                <Icon.FileText size={50} color={green} />
+                                <CardTitle>{license.business_name}</CardTitle>
+                            </CardView>
+                            <Line />
+                        </FittedContainer>
+                    ))}
                 </ListsView>
             </InnerContainer>
         </Container >
