1365b858258acabd2673da1ecf68d392b34e53e5
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-06 04:22:26 +0300

    Update Home Screen

--- screens/home.js
+++ screens/home.js
@@ -1,17 +1,38 @@
-import React, { useContext } from 'react'
+import React, { useEffect, useCallback, useContext, useState } from 'react'
+import { ActivityIndicator } from 'react-native'
+import { useDispatch, useSelector } from 'react-redux'
 import { StatusBar } from 'expo-status-bar'
 import * as Icon from 'react-native-feather';
-// import AsyncStorage from '@react-native-async-storage/async-storage';
-
-import { Container, InnerContainer, CardContainer, CardView, CardTitle, Colors, Line, PageTitle, SubHeading,
-ListsView
+import {
+  Container, InnerContainer, CardContainer, CardView, CardTitle, Colors, Line, PageTitle, SubHeading,
+  ListsView, FittedContainer
 } from '../components/styles'
-// import { UserContext } from '../components/UserContext';
+import { UserContext } from '../components/UserContext';
+import { getAllLicenses } from '../API/licenseRequests';
+import { filteredLicenses } from '../redux/selectors';
+import { saveLicenses } from '../redux/licenseSlice';
 
 const { green, platinum } = Colors
 
 const Home = ({ navigation }) => {
-  // const { userData, setUserData } = useContext(UserContext);
+  const [loading, setLoading] = useState(false);
+  const { userData } = useContext(UserContext);
+  const dispatch = useDispatch();
+  const dueLicenses = useSelector(filteredLicenses);
+
+  const setLicenseState = (incomingLicenses) => {
+    dispatch(saveLicenses(incomingLicenses));
+  }
+
+  const fetchLicenses = useCallback(() => {
+    setLoading(true);
+    getAllLicenses(userData.refreshToken, setLoading, setLicenseState)
+  }, [userData, dispatch]);
+
+  useEffect(() => {
+    fetchLicenses();
+  }, [fetchLicenses]);
+
   return (
     <Container isWhite={true}>
       <StatusBar style="dark" />
@@ -32,9 +53,26 @@ const Home = ({ navigation }) => {
             <CardTitle>Account</CardTitle>
           </CardView>
           <Line />
-        </CardContainer>        
+        </CardContainer>
         <ListsView>
-          <SubHeading color={platinum} >Upcoming Payments</SubHeading>
+          <SubHeading color={platinum} >Due Renewals</SubHeading>
+          {loading ? (
+            <ActivityIndicator size="large" color={green} />
+          ) : (
+            <>
+              {dueLicenses.length > 0 && dueLicenses.map((license, i) => (
+                <FittedContainer key={i}>
+                  {/* <Line /> */}
+                  {/* <CardView onPress={() => navigation.navigate('License', { license })}> */}
+                  <CardView>
+                    <Icon.FileText size={50} color={green} />
+                    <CardTitle>{license.business_name}</CardTitle>
+                  </CardView>
+                  <Line />
+                </FittedContainer>
+              ))}
+            </>
+          )}
         </ListsView>
       </InnerContainer>
     </Container>
