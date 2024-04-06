7241547a98caed4e0e183e57f3e2cf932e68faf2
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-06 07:53:46 +0300

    Update Licenses Screen

--- screens/Licenses.js
+++ screens/Licenses.js
@@ -1,20 +1,38 @@
 import React, { useState } from 'react'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 import * as Icon from 'react-native-feather'
 import { Container, InnerContainer, PageTitle, SubHeading, ListsView, Line, Colors, CardContainer, FittedContainer,
     FloatingButton, StyledButton, ButtonText, CardView, CardTitle 
 } from '../components/styles'
 import LicenseForm from '../components/LicenseForm';
+import DueList from '../components/DueList';
+import { addLicense } from '../redux/licenseSlice';
 
-const { green, platinum } = Colors;
+const { green, platinum, red, amber } = Colors;
 
 const Licenses = () => {
     const [newLicense, setNewLicense] = useState(false)
     const Licenses = useSelector(state => state.licenses)
+    const dispatch = useDispatch()
 
-    const closeForm = () => {
-        setNewLicense(false)
+    const closeForm = (license) => {
+        setNewLicense(false);
+        dispatch(addLicense(license));
     }
+
+    const handleColorAlerts = (expiry_date) => {
+        const expiryDate = new Date(expiry_date);
+        const today = new Date();
+        const timeDiff = expiryDate - today;
+        const daysDiff = Math.ceil(timeDiff / (1000 * 60 * 60 * 24));
+        console.log(`Days diff: ${daysDiff}, Expiry Date: ${expiryDate}`)
+        if (daysDiff <= 0) {
+          return red;
+        } else if (daysDiff <= 30) {
+        return amber;
+        }
+        return;
+      }
 
     return (
         <Container >
@@ -38,17 +56,14 @@ const Licenses = () => {
                     <Line />
                 </CardContainer>
                 <ListsView>
-                    <SubHeading color={platinum} >Saved Licenses</SubHeading>
+                    <SubHeading color={green} >Saved Licenses</SubHeading>
                     {Licenses.length > 0 && Licenses.map((license, i) => (
-                        <FittedContainer key={i}>
-                            {/* <Line /> */}
-                            {/* <CardView onPress={() => navigation.navigate('License', { license })}> */}
-                            <CardView>
-                                <Icon.FileText size={50} color={green} />
-                                <CardTitle>{license.business_name}</CardTitle>
-                            </CardView>
-                            <Line />
-                        </FittedContainer>
+                        <DueList 
+                            key={i}
+                            color={handleColorAlerts(license.expiry_date)}                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                        
+                            businessName={license.business_name} 
+                            expiryDate={license.expiry_date} 
+                        />
                     ))}
                 </ListsView>
             </InnerContainer>
