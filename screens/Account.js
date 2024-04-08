0f382382f951de7f95ef12791e84524fea65027b
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-08 23:32:43 +0300

    Update Account Screen

--- screens/Account.js
+++ screens/Account.js
@@ -2,34 +2,20 @@ import React, { useState, useContext } from 'react'
 import { useDispatch } from 'react-redux'
 import { ActivityIndicator } from 'react-native'
 import * as Icon from 'react-native-feather'
-import { Container, InnerContainer, PageTitle, CardContainer, Line, StyledButton, ButtonText, Colors } from '../components/styles'
-import AsyncStorage from '@react-native-async-storage/async-storage'
+import { Container, InnerContainer, PageTitle, CardContainer, Line, CardView, ButtonText, Colors } from '../components/styles'
 import { UserContext } from '../components/UserContext'
 import { logoutUser } from '../API/userRequests'
-import { clearLicenses } from '../redux/licenseSlice'
-
 const { green } = Colors
 
 const Account = ({ navigation }) => {
   const [loading, setLoading] = useState(false)
-  const dispatch = useDispatch(clearLicenses())
+  const dispatch = useDispatch()
   const { userData, setUserData } = useContext(UserContext);
   const { id_number } = userData
 
-  const clearUserData = () => {
-    AsyncStorage.removeItem('userSessionData')
-      .then(() => {
-        dispatch(clearLicenses())
-        setUserData(null)
-      })
-      .catch((err) => {
-        console.log(err)
-      })
-  }
-
   const handleLogout = () => {
     setLoading(true);
-    logoutUser(id_number, clearUserData, setLoading);
+    logoutUser(id_number, setLoading, dispatch, setUserData);
   }
 
   return (
@@ -41,10 +27,10 @@ const Account = ({ navigation }) => {
         ) : (
           <CardContainer>
             <Line />
-            <StyledButton onPress={handleLogout} style={{ flexDirection: 'row' }}>
+            <CardView onPress={handleLogout} style={{ flexDirection: 'row' }}>
               <Icon.LogOut size={50} color={green} />
               <ButtonText>Log Out</ButtonText>
-            </StyledButton>
+            </CardView>
             <Line />
           </CardContainer>
         )}
