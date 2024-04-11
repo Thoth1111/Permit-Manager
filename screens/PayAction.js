8b6532c8c32ffbf10824988e4a3e66bc9e3d9eed
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-11 17:13:47 +0300

    Update PayAction Screen

--- screens/PayAction.js
+++ screens/PayAction.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { SelectList } from 'react-native-dropdown-select-list'
 import { Container, InnerContainer, Colors } from '../components/styles';
 
+const { green, yellow } = Colors;
+
 const PayAction = () => {
+    const Licenses = useSelector(state => state.licenses)
+    const selectionList = Licenses.map(license => {
+        return {
+            value: license.business_name,
+            id: license._id
+        }
+    })
+    const [newPayment, setNewPayment] = useState({
+        license_id: '',
+        extension_period: '',
+        phone_number: '',
+        amount: '',
+    })
+
     return (
         <Container>
             <InnerContainer>
-                
+                <SelectList
+                    placeholder="Choose License"
+                    data={selectionList}
+                    setSelected={(selected) => {
+                        setNewPayment({
+                            ...newPayment,
+                            license_id: selected.id
+                        })
+                    }}
+                />
             </InnerContainer>
         </Container>
     )
 }
+
+export default PayAction;
