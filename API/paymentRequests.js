90832e8d3691763c42ac537d8e8b67264fb734a6
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-17 14:23:08 +0300

    Update PaymentRequests to include get payments

--- API/paymentRequests.js
+++ API/paymentRequests.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 import { logoutUser } from './userRequests';
 import { getAllLicenses } from './licenseRequests';
+import { addPayment, savePayments } from '../redux/paymentSlice';
 // Make a Safaricom Payment
 const initiatePayment = async (newPayment, userData, setLoading, dispatch, setUserData, redirectToHome) => {
     await axios.post('https://clm-server.onrender.com/payment/saf/pay', newPayment,
@@ -11,6 +12,7 @@ const initiatePayment = async (newPayment, userData, setLoading, dispatch, setUs
     })
     .then((res) => {
         getAllLicenses(userData, setLoading, dispatch, setUserData)
+        dispatch(addPayment(res.data.payment))
         alert('Payment successful')
         redirectToHome()
     })
@@ -25,4 +27,27 @@ const initiatePayment = async (newPayment, userData, setLoading, dispatch, setUs
     })
 }
 
-export { initiatePayment };
+const getPayments = async (userData, setLoading, dispatch, setUserData) => {
+    console.log('Getting payments')
+    await axios.get('https://clm-server.onrender.com/payment/receipts',
+    {
+        headers: {
+            Authorization: userData.refreshToken
+        }
+    })
+    .then((res) => {
+        console.log(res.status)
+        dispatch(savePayments(res.data.payments))
+    })
+    .catch((err) => {
+        if(err.response.status === 401 || err.response.status === 403) {
+            logoutUser(userData.id_number, setLoading, dispatch, setUserData)
+        }
+        console.log(err)
+    })
+    .finally(() => {
+        setLoading(false)
+    })
+}
+
+export { initiatePayment, getPayments };
