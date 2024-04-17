ae34989267c76290523178c84a7672acd2e85ce7
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-17 14:21:07 +0300

    Update Redux store to include payment reducer

--- redux/store.js
+++ redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import licenseReducer from './licenseSlice.js';
+import paymentReducer from './paymentSlice.js';
 
 const store = configureStore({
     reducer: {
         licenses: licenseReducer,
+        payments: paymentReducer
         // notificationState: notificationReducer
     },
 });
