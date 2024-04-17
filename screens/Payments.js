42f23aa505486ec3bdf3f4ef6abeac2bfd1162d0
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-17 14:18:00 +0300

    Update Payments Screen

--- screens/Payments.js
+++ screens/Payments.js
@@ -1,10 +1,14 @@
 import React from 'react'
+import { useSelector } from 'react-redux'
 // Styled Components
 import { Container, InnerContainer, CardContainer, Line, CardView, ButtonText, ListsView, SubHeading, Colors } from '../components/styles'
+import PayView from '../components/PayView'
 
 const { green } = Colors
 
 const Payments = ({ navigation }) => {
+    const payments = useSelector(state => state.payments)
+
     return (
         <Container >
             <InnerContainer>
@@ -17,6 +21,16 @@ const Payments = ({ navigation }) => {
                 </CardContainer>
                 <ListsView>
                     <SubHeading color={green}>Transactions</SubHeading>
+                    {payments.length > 0 && payments.map((payment, i) => (
+                        <PayView
+                            key={i}
+                            businessName={payment.business_name}
+                            _id={payment._id}
+                            transactionDate={payment.transaction_date}
+                            amount={payment.amount}
+                            navigation={navigation}
+                        />
+                    ))}
                 </ListsView>
             </InnerContainer>
         </Container>
