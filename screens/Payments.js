e6daffaeb3fdbad06626f23e6b6cabe505a965db
Alwyn Androvelle <alwynsimiyu@gmail.com>
2024-04-22 17:42:01 +0300

    Update Payment Screen

--- screens/Payments.js
+++ screens/Payments.js
@@ -1,13 +1,29 @@
-import React from 'react'
-import { useSelector } from 'react-redux'
+import React, { useContext, useEffect, useState } from 'react'
+import { ActivityIndicator } from 'react-native';
+import { useDispatch, useSelector } from 'react-redux'
 // Styled Components
 import { Container, InnerContainer, CardContainer, Line, CardView, ButtonText, ListsView, SubHeading, Colors } from '../components/styles'
-import PayView from '../components/PayView'
+import PayView from '../components/PayView';
+import { getPayments } from '../API/paymentRequests'
+import { UserContext } from '../components/UserContext'
+
 
 const { green } = Colors
 
 const Payments = ({ navigation }) => {
-    const payments = useSelector(state => state.payments)
+    const payments = useSelector(state => state.payments).sort((a, b) => new Date(b.expiry_date) - new Date(a.expiry_date));
+    const { userData, setUserData } = useContext(UserContext);
+    const [loading, setLoading] = useState(false);
+    const dispatch = useDispatch();
+
+    const fetchPayments = async () => {
+        setLoading(true);
+        await getPayments(userData, setLoading, dispatch, setUserData)
+    }
+
+    useEffect(() => {
+        fetchPayments();
+    }, [])
 
     return (
         <Container >
@@ -21,16 +37,22 @@ const Payments = ({ navigation }) => {
                 </CardContainer>
                 <ListsView>
                     <SubHeading color={green}>Transactions</SubHeading>
-                    {payments.length > 0 && payments.map((payment, i) => (
-                        <PayView
-                            key={i}
-                            businessName={payment.business_name}
-                            _id={payment._id}
-                            transactionDate={payment.transaction_date}
-                            amount={payment.amount}
-                            navigation={navigation}
-                        />
-                    ))}
+                    {loading ? (
+                        <ActivityIndicator size="large" color={green} />
+                    ) : (
+                        <>
+                            {payments.length > 0 && payments.map((payment, i) => (
+                                <PayView
+                                    key={i}
+                                    businessName={payment.business_name}
+                                    _id={payment._id}
+                                    transactionDate={payment.transaction_date}
+                                    amount={payment.amount}
+                                    navigation={navigation}
+                                />
+                            ))}
+                        </>
+                    )}
                 </ListsView>
             </InnerContainer>
         </Container>
